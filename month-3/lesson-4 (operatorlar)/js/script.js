b41b8563534a9c41a9e8fa7e40f7344c4f4b58b4
Tohirjon-Odilov <82634626+Tohirjon-Odilov@users.noreply.github.com>
2022-08-12 22:40:37 +0500

    Add lesson - 4

---
+++ month-3/lesson-4 (operatorlar)/js/script.js
@@ -0,0 +1,33 @@
+let userName = "Tohirjon"
+console.log(userName);
+
+let userAge = 25;
+console.log(userAge);
+
+let num = 123n;
+let num2 = 123e5;
+console.log(num);
+console.log(num2);
+
+let userHasCar = true;
+console.log(userHasCar);
+
+let userLocation = null;
+console.log(userLocation);
+
+let hasComputer = undefined;
+console.log(hasComputer);
+
+let smb = Symbol("smb");
+console.log(smb);
+
+let obj = {
+    name: "Tohirjon",
+    age: undefined,
+    hasCar: true,
+    location: null,
+};
+console.log(obj);
+
+
+alert("salom bolalar")
