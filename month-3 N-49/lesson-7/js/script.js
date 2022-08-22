25b32ea25e7152662758cdc885db00c330e2f824
Tohirjon-Odilov <82634626+Tohirjon-Odilov@users.noreply.github.com>
2022-08-22 12:53:10 +0500

    Update script.js

--- month-3 N-49/lesson-7/js/script.js
+++ month-3 N-49/lesson-7/js/script.js
@@ -7,6 +7,16 @@
 // }
 
 // Task 2
+// let num1 = 5;
+// let num2 = 15;
+
+// for (let i = num1; i <= num2; i++) {
+// 	let num3 = i.length;
+// 	// console.log(num3);
+// 	console.log(i, num2[i]);
+// }
+
+// Task 3
 let num1 = 5;
 let num2 = 15;
 
@@ -15,5 +25,3 @@ for (let i = num1; i <= num2; i++) {
 	// console.log(num3);
 	console.log(i, num2[i]);
 }
-
-// Task 3
