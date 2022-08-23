0887bc411e8157b6dbf806f36cdb8a1180dfeaea
Tohirjon-Odilov <82634626+Tohirjon-Odilov@users.noreply.github.com>
2022-08-23 21:12:36 +0500

    add lesson - 9 recursion and prototype

---
+++ month-3 N-49/lesson-9 (recursion, prototype)/js/script.js
@@ -0,0 +1,83 @@
+// helloga necha son berilsa shuncha hello chiqadi
+
+// function hello(num) {
+// 	console.log("hello");
+// 	let newNum = num - 1;
+// 	// hello(num);
+// 	if (newNum > 0) {
+// 		hello(newNum);
+// 	}
+// }
+
+// hello(9);
+
+// function Car(brend, fast, color, price) {
+// 	this.brend = brend;
+// 	this.fast = fast;
+// 	this.color = color;
+// 	this.price = price;
+// }
+
+// Car.prototype.getInfo = function () {
+// 	console.log(
+// 		`${this.brend} tezligi ${this.fast} km/h. Rangi ${this.color} va narxi $${this.price} mln.`
+// 	);
+// };
+
+// let car1 = new Car("BMW", 900, "qora", 200000);
+// car1.getInfo();
+
+// let car2 = new Car("AUDI", 700, "qizil", 120000);
+// car2.getInfo();
+
+// !sdklfjlksdjfksjdfjsdfk
+
+// function slice() {
+// 	let str = "hello";
+// 	let counter = 0;
+// 	let obj = {};
+
+// 	for (let i = 0; i < str.length; i++) {
+// 		counter++;
+// 		// obj.key = str[i];
+// 		obj.counter = i;
+// 		obj.key = str[i];
+// 		console.log(obj);
+// 	}
+// }
+
+// slice();
+
+// function slice() {
+// 	let str = "hello";
+// 	let counter = 0;
+// 	let obj = {};
+
+// 	for (let i = 0; i < str.length; i++) {
+// 		counter++;
+// 		obj.key = str[i];
+// 		console.log(obj);
+// 	}
+// }
+
+// slice();
+
+// let obj = {};
+// function slice() {
+// 	let str = "hello";
+// 	let counter = 0;
+
+// 	for (let i = 0; i < str.length; i++) {
+// 		counter++;
+// 		obj.counter = counter;
+// 		obj.key = str[i];
+// 		for (let i in str) {
+// 			obj.key = str[i];
+// 			// console.log(i);
+// 		}
+// 	}
+// 	// console.log(" ");
+// 	console.log(obj);
+// }
+
+// slice();
