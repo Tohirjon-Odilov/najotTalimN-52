e92306d36d684d39b77f44533b9c05f34e6ea5b1
Tohirjon-Odilov <82634626+Tohirjon-Odilov@users.noreply.github.com>
2022-08-26 12:04:38 +0500

    Add break and continus statement

--- month-3 N-49/lesson-6 (for loop)/js/script.js
+++ month-3 N-49/lesson-6 (for loop)/js/script.js
@@ -16,8 +16,8 @@
 
 // ? Foydalanuvchi son kiritadi har kiritgan sonini bir biriga qo'shib ketadi va qachonki 0 kiritsa amal to'xtaydi va consoleda barcha sonlar yig'indisi chiqadi.
 
-let result = 0;
-control = true;
+// let result = 0;
+// control = true;
 
 // while (control) {
 // 	let num = parseInt(prompt("Enter a number."));
@@ -31,12 +31,35 @@ control = true;
 
 // *Do while bilan
 
-do {
-	let num = parseInt(prompt("Enter a number."));
-	if (num !== 0) {
-		result += num;
-	} else {
-		control = false;
-		console.log(result);
+// do {
+// 	let num = parseInt(prompt("Enter a number."));
+// 	if (num !== 0) {
+// 		result += num;
+// 	} else {
+// 		control = false;
+// 		console.log(result);
+// 	}
+// } while (control);
+
+/*********
+ * *BREAK *
+ *********/
+
+let num = 20 - 0,
+	i = 0;
+
+// while (i <= 100) {
+// 	i++;
+// 	console.log(i);
+// 	if (i === 8) {
+// 		break;
+// 	}
+// }
+
+while (i < 20) {
+	i++;
+	if (i % 2 === 0) {
+		continue;
 	}
-} while (control);
+	console.log(i);
+}
