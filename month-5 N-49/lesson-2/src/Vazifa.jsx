6924341bfd3bb31489665a131025ffd124295e3f
Tohirjon-Odilov <shahanshoh819@gmail.com>
2022-10-13 09:12:07 +0500

    update files

---
+++ month-5 N-49/lesson-2/src/Vazifa.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function Vazifa() {
+  return (
+    function HelloWorld() {
+      return React.createElement("div", { className: "container", onClick: () => console.log("salom") },
+        React.createElement("h2", null, "Hello"),
+        React.createElement("p", null, "Boshqa so'zlar"),
+        React.createElement("div", { className: "box" }, React.createElement("span", null, "inner1"), React.createElement("span", null, "inner2")));
+
+
+
+      // function HelloWorld() {
+      //   return (
+      //     <div className="container" onClick={() => console.log("salom")}>
+      //       <h2>Hello</h2>
+      //       <p>Boshqa so'zlar</p>
+      //       <div className="box">
+      //         <span>inner1</span>
+      //         <span>inner2</span>
+      //       </div>
+      //     </div>
+      //   );
+      // }
+    }
+  )
+}
+
+export default Vazifa();
