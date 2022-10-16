919c57a8ca5d2b08d4ea0d73008901f6fb5739b3
Tohirjon-Odilov <shahanshoh819@gmail.com>
2022-10-16 15:05:36 +0500

    Update files and add lesson - 4

--- month-5 N-49/lesson-2/src/components/Vazifa.jsx
+++ month-5 N-49/lesson-2/src/components/Vazifa.jsx
@@ -6,7 +6,11 @@ function Vazifa() {
       return React.createElement("div", { className: "container", onClick: () => console.log("salom") },
         React.createElement("h2", null, "Hello"),
         React.createElement("p", null, "Boshqa so'zlar"),
-        React.createElement("div", { className: "box" }, React.createElement("span", null, "inner1"), React.createElement("span", null, "inner2")));
+        React.createElement("div", { className: "box" },
+          React.createElement("span", null, "inner1"), 
+          React.createElement("span", null, "inner2"),
+        ),
+      );
 
 
 
--- month-5 N-49/lesson-2/src/index.js
+++ month-5 N-49/lesson-2/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Vazifa from './Vazifa';
+import Vazifa from './components/Vazifa';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
