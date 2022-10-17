6d1bba23744bceb4319796af7b7eac2f940bd96f
Tohirjon-Odilov <shahanshoh819@gmail.com>
2022-10-17 11:43:52 +0500

    rename lesson-2

--- month-5 N-49/lesson-2 (m-header)/src/index.js
+++ month-5 N-49/lesson-2 (m-header)/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import Vazifa from './components/Vazifa';
-import Luyboy from './components/navbar/Navbar';
+import Qushimcha from './components/navbar/Navbar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <Vazifa /> */}
-    <Luyboy />
+    <Qushimcha />
   </React.StrictMode>
 );
