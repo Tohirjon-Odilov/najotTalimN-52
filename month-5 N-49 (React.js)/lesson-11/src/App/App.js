df6912ebffe76b81ef06a3e86580a8c167624a37
Tohirjon-Odilov <shahanshoh819@gmail.com>
2022-11-03 16:08:21 +0500

    updates files

--- month-5 N-49 (React.js)/lesson-11/src/App/App.js
+++ month-5 N-49 (React.js)/lesson-11/src/App/App.js
@@ -1,29 +1,29 @@
 import React, { useEffect, useRef } from 'react'
-// import Modal from "../UI/Modal/Modal"
-import { UseStore } from "../UseStore/UseStore"
+import Modal from "../UI/Modal/Modal"
+// import { UseStore } from "../UseStore/UseStore"
 import './App.css'
 
 function App() {
-  // const form = e => {
-  // e.preventDefault()
-  // 
-  // }
+  const form = e => {
+    e.preventDefault()
+
+  }
   const btn = useRef()
   useEffect(() => {
     console.log(btn);
   }, [])
 
-  const [state, dispatch] = UseStore()
+  // const [state, dispatch] = UseStore()
 
   return (
     <div className="container">
-      <button onClick={() => dispatch({ type: "counter" })} ref={btn}>
-        click - {state}
+      <button ref={btn}>
+        click
       </button>
-      {/* <Modal> */}
-      {/* <form onClick={form} > */}
-      {/* </form> */}
-      {/* </Modal> */}
+      <Modal>
+        <form onClick={form} >
+        </form>
+      </Modal>
     </div>
   )
 }
